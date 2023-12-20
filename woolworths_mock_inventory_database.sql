5a99d8fd3ed9806dab3ae14dc9987ff945d42acc
sicelwesihle m <123302904+sicelwesihle@users.noreply.github.com>
2023-12-20 21:21:37 +0200

    Add files via upload
    mock_up database

---
+++ woolworths_mock_inventory_database.sql
@@ -0,0 +1,14 @@
+-- Database: Woolworthss
+
+-- DROP DATABASE IF EXISTS "Woolworthss";
+
+CREATE DATABASE "Woolworthss"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_South Africa.1252'
+    LC_CTYPE = 'English_South Africa.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
